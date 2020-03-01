1b13cabf0523a0a8b72b441c5f616f49e7f47442
alberto <alber@DESKTOP-QDPFLHT.home>
2020-03-01 16:05:22 +0100

    version 1.6

--- src/Principal.java
+++ src/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
 //COCHEEE
 	public static void main(String[] args) {
 		//Coche de Pilar
-		Coche cochePilar = new Coche("5544PJk", "Tesla", "Model X", "White" );
+		Coche cochePilar = new Coche("4427QVk", "Citroen", "Model X", "Red" );
 		
 		cochePilar.acelerar();
 		cochePilar.acelerar();
